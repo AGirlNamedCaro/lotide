d9fdcf88fdaac03ac46e0cfb7783def057ae9db0
Carolina Contreras Issa <agirlnamedcaro@gmail.com>
2020-01-07 22:51:52 +0000

    eslint

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log("Assertion Passed: " + actual + " === " + expected + " ✅");
+    console.log(`Assertion Passed: ${actual} === ${expected}   ✅`);
   } else {
-    console.log("Assertion Failed:" + [actual] + " !== " + [expected] + " 🛑");
+    console.log(`Assertion Failed: ${actual} !== ${expected}   🛑`);
   }
   
 };
