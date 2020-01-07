9232a695a8eb099b03c3a3786a9b7af120369c13
Carolina Contreras Issa <agirlnamedcaro@gmail.com>
2020-01-07 23:56:46 +0000

    Created a file that returns the two middle numbers in an array

---
+++ middle.js
@@ -0,0 +1,69 @@
+const eqArrays = function(array1, array2) {
+
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i ++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+
+  const result = eqArrays(actual, expected);
+
+  if (result === true) {
+    //console.log(`Assertion Passed: ${array1} === ${array2}   ✅`);
+    return `Assertion Passed: ${actual} === ${expected}   ✅`;
+
+  } else {
+    // console.log(`Assertion Failed: ${array1} !== ${array2}   🛑`);
+    return `Assertion Failed: ${actual} !== ${expected}  🛑`;
+  }
+
+};
+
+const middle = function(array) {
+  let array1 = [];
+  let sum = 0;
+  let result = 0;
+  let midIndex;
+  let midIndex2;
+
+  if (array.length === 1 || array.length === 2) {
+    return array1;
+  } else if (array.length % 2 !== 0) {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+
+    result = sum / array.length;
+    array1.push(result);
+    return array1;
+  } else {
+
+    midIndex = Math.floor(array.length / 2);
+    array1.push(midIndex);
+    
+    for (let i = 0; i < array.length; i ++) {
+      if (midIndex === array[i]) {
+        midIndex2 = array[i + 1];
+      }
+    }
+    array1.push(midIndex2);
+
+    return array1;
+
+  }
+};
+// console.log(assertArraysEqual(middle([1]),[]));
+console.log(assertArraysEqual(middle([2]), []));
+console.log(assertArraysEqual(middle([1,2,3]), [2]));
+console.log(assertArraysEqual(middle([1,2,3,4]), [2,3]));
+console.log(assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]));
+
+
