46ac40a3edd99e76cfcab3830a5b2686c19176d9
Carolina Contreras Issa <agirlnamedcaro@gmail.com>
2020-01-07 23:11:53 +0000

    Created a function that asserts wether arrays are equal or not

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,31 @@
-const eqArrays = function (array1, array2) {
+const eqArrays = function(array1, array2) {
 
-  if(array1.length === array2.length) {
-    for(let i = 0; i < array1.length; i ++) {
-      if(array1[i] !== array2[i]) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i ++) {
+      if (array1[i] !== array2[i]) {
         return false;
       }
     }
     return true;
-  }
-  else {
+  } else {
     return false;
   }
+};
 
- 
-}
+const assertArraysEqual = function(array1, array2) {
+
+  const result = eqArrays(array1, array2);
+
+  if (result === true) {
+    //console.log(`Assertion Passed: ${array1} === ${array2}   ✅`);
+    return `Assertion Passed: ${array1} === ${array2}   ✅`;
+
+  } else {
+
+    // console.log(`Assertion Failed: ${array1} !== ${array2}   🛑`);
+    return `Assertion Failed: ${array1} !== ${array2}  🛑`;
+  }
+
+};
+
+console.log(assertArraysEqual([1,2,3],[1,2,3]));
