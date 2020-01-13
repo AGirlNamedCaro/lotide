a8e2bc3310e0459c481cc646252c2f63e0842c09
Carolina Contreras Issa <agirlnamedcaro@gmail.com>
2020-01-13 16:54:16 +0000

    Refactored middle and created its test file

--- middle.js
+++ middle.js
@@ -60,10 +60,7 @@ const middle = function(array) {
 
   }
 };
-// console.log(assertArraysEqual(middle([1]),[]));
-console.log(assertArraysEqual(middle([2]), []));
-console.log(assertArraysEqual(middle([1,2,3]), [2]));
-console.log(assertArraysEqual(middle([1,2,3,4]), [2,3]));
-console.log(assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]));
 
+
+module.exports = middle;
 
