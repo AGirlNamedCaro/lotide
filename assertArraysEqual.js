bdc5e15d84c54136f1f7a63d0fea4faf762a2bb1
Carolina Contreras Issa <agirlnamedcaro@gmail.com>
2020-01-13 16:44:57 +0000

    Refactored assertArraysEqual and created its test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(array1, array2) {
-
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i ++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
 
@@ -28,4 +16,5 @@ const assertArraysEqual = function(actual, expected) {
 
 };
 
-console.log(assertArraysEqual([1,2,3],[1,2,3]));
+
+module.exports = assertArraysEqual;
